14943b8ad03af75eeb73b94cc592c9ec0cd26b9e
Adam Farden <adam@farden.cz>
2015-01-03 02:57:39 +0100

    rhine: Add FXP's dual boot and recovery solution
    Change-Id: I33bbdd133ae30b5b09d0b9009870d83603b64350

--- device.mk
+++ device.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     e2fsck
 
+# Extract recovery ramdisks
+PRODUCT_PACKAGES += \
+    extract_elf_ramdisk
+
 # Platform specific tags
 #
 PRODUCT_TAGS += dalvik.gc.type-precise
